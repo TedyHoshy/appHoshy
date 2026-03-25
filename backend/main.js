0bda9ec145dd5049f4ed4dcb99c128fb7f925076
Brenanyelit Ledezma <yorianyjr@gmail.com>
2026-03-25 17:03:40 -0500

    Mariposa

--- backend/main.js
+++ backend/main.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import cors from 'cors';
-import  userReducer  from './routes/userRoutes.js';
-import { fileURLToPath } from 'url'
+import userRoutes from './routes/userRoutes.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-//servidor archivos estaticos del frontend
+// servidor archivos estáticos del frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/users', userRoutes);
 
-app.listen(300, () => {
-    console.log('Servidor en http://localhost:3000')
+app.listen(3000, () => {
+    console.log('Servidor en http://localhost:3000');
 });
